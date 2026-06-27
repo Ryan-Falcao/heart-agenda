cea24e3cd4b900884da5203d75750b58184212f3
Ryan Marques Monteiro Falcão <ryanmarques2007f@gmail.com>
2026-06-27 11:21:07 -0300

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,7 @@
 
+import { createStart, createMiddleware } from "@tanstack/react-start";
 import { renderErrorPage } from "./lib/error-page";
 import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
-
 const errorMiddleware = createMiddleware().server(async ({ next }) => {
   try {
     return await next();
@@ -16,7 +16,6 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
     });
   }
 });
-
 export const startInstance = createStart(() => ({
   functionMiddleware: [attachSupabaseAuth],
   requestMiddleware: [errorMiddleware],
