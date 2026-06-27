7f647729713ebb97e60fa72b4f97c83a5683d11d
Ryan Marques Monteiro Falcão <ryanmarques2007f@gmail.com>
2026-06-27 11:10:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { nitro } from "nitro/vite";
+
 export default defineConfig({
+  vite: {
+    plugins: [
+      nitro({
+        preset: "vercel",
+      }),
+    ],
+  },
   tanstackStart: {
     server: { entry: "server" },
-  },
-  nitro: {
-    preset: "vercel",
   },
 });
